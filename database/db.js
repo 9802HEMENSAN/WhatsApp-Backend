b08e0441b4af198812e47c4089de9d9c0fe6a7b2
9802HEMENSAN <hmahilange9802@gmail.com>
2023-06-19 11:59:46 +0530

    whatsapp clone

---
+++ database/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const Connection = async () => {
+ 
+    try {
+        await mongoose.connect(process.env.MONGO_URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
+        console.log('Database Connected Succesfully');
+    } catch(error) {
+        console.log('Error: ', error.message);
+    }
+
+};
+
+export default Connection;
---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import Connection from './database/db.js';
+import Routes from './routes/Routes.js';
+
+
+dotenv.config();
+const app = express();
+ 
+Connection();
+
+app.listen(process.env.PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/', Routes);
